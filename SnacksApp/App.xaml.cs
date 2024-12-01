9edf7c353f5512735f97969ed70c9631d4f6b355
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-12-01 17:49:13 +0000

    Add project files.

---
+++ SnacksApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using SnacksApp.Pages;
+using SnacksApp.Services;
+
+namespace SnacksApp
+{
+    public partial class App : Application
+    {
+        private readonly ApiService _apiService;
+
+        public App(ApiService apiService)
+        {
+            InitializeComponent();
+            _apiService = apiService;
+            MainPage = new NavigationPage(new InscricaoPage(_apiService));
+
+        }
+    }
+}
